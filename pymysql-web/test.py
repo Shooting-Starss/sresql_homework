ebe2581b2b9224b50b546f59aeef62662649bf0d
Shooting-Starss <1073518250@qq.com>
2023-01-08 14:01:47 +0800

    登录

--- pymysql-web/test.py
+++ pymysql-web/test.py
@@ -1,13 +1,13 @@
 import requests
 
 # 注册测试
-# url = 'http://127.0.0.1:8000/register'
+# url = 'http://127.0.0.1:7000/register'
 # params = {"account_num": "2125488", "account_name": "xunjiangzee", "account_pass": "1234567", "entry_date":"2023-1-2"}
 # result1 = requests.post(url, params=params)
 # print(result1.text)
 
 # # # 查询测试
-# url = 'http://127.0.0.1:8000/get_info'
+# url = 'http://127.0.0.1:7000/get_info'
 # params = {"account_name": "xunjiangze"}
 # result2 = requests.get(url, params)
 # print(result2.json())
